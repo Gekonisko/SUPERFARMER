b7e7ebf6ff7a3fa0f99daf8fe5d0c4ada02bd398
Gekon <jakubocytko@student.agh.edu.pl>
2024-05-12 02:09:22 +0200

    Add mechanic to adding animals to farm

--- Assets/Scripts/DiceRollerView.cs
+++ Assets/Scripts/DiceRollerView.cs
@@ -1,24 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DiceRollerView : MonoBehaviour
 {
     [SerializeField] private GameObject _dices;
+    [SerializeField] private Button _nextButton;
+
+    private int _rollDiceCounter = 0;
 
     private void Awake()
     {
         GameManager.ShowDiceRoller += OnShowDiceRoller;
+        DiceView.RollDice += OnRollDice;
     }
 
     private void OnShowDiceRoller(bool isActive)
     {
         _dices.SetActive(isActive);
+        _nextButton.interactable = false;
+    }
+
+    private void OnRollDice(Animal animal)
+    {
+        _rollDiceCounter += 1;
+        if (_rollDiceCounter == 2)
+        {
+            _rollDiceCounter = 0;
+            _nextButton.interactable = true;
+        }
     }
 
     private void OnDestroy()
     {
         GameManager.ShowDiceRoller -= OnShowDiceRoller;
+        DiceView.RollDice -= OnRollDice;
     }
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -8,8 +8,22 @@ public class GameManager : MonoBehaviour
     public static event Action<bool> ShowMenu;
     public static event Action<bool> ShowDiceRoller;
 
+    private static GameManager _instance;
+    public static GameManager Instance { get { return _instance; } }
+
+    public int dices = 2;
+
     private void Awake()
     {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+
         ThrowDiceButton.Click += OnThrowDice;
         BackToMenuButton.Click += OnShowMenu;
     }
