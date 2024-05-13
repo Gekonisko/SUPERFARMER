698cd245cd0999b9db07092e19c16ca252318862
Gekon <jakubocytko@student.agh.edu.pl>
2024-05-13 22:31:23 +0200

    Refactor

--- Assets/Scripts/UI/Menu/DefinePlayers/DefinePlayersView.cs
+++ Assets/Scripts/UI/Menu/DefinePlayers/DefinePlayersView.cs
@@ -4,80 +4,76 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using Zenject;
 
-namespace DefinePlayers
-{
+namespace DefinePlayers {
 
-    public class DefinePlayersView : MonoBehaviour
-    {
+    public class DefinePlayersView : MonoBehaviour {
+
         [SerializeField] private GameObject[] players;
+        [SerializeField] private GameObject addPlayer;
+        [SerializeField] private GameObject removePlayer;
         private int playersNumber = 2;
 
         [Inject]
         private GameService gameService;
 
-        private void Awake()
-        {
+        private void Awake() {
             DefinePlayers.PlayButton.Click += OnPlayButton;
         }
 
-        private void Start()
-        {
+        private void Start() {
             var i = 0;
             players.ToList().ForEach(p => p.SetActive(i++ < playersNumber));
+            removePlayer.SetActive(false);
         }
 
-        public void OnPlayButton()
-        {
+        public void OnPlayButton() {
             AddPlayers();
             ChangeSceneButton.OnClick("Game");
         }
 
-        private void AddPlayers()
-        {
-            for (int i = 0; i < playersNumber; i++)
-            {
-                if (players[i].activeInHierarchy)
-                {
-                    gameService.AddPlayer(players[i].GetComponentInChildren<TMP_InputField>().text);
+        private void AddPlayers() {
+            for (int i = 0; i < playersNumber; i++) {
+                if (players[i].activeInHierarchy) {
+                    string playerName = players[i].GetComponentInChildren<TMP_InputField>().text;
+                    gameService.AddPlayer(playerName.Equals("") ? $"Player {i+1}" : playerName);
                 }
             }
         }
 
-
-        public void AddPlayerInput()
-        {
-            if (playersNumber < gameService.MAX_Players)
-            {
+        public void AddPlayerInput() {
+            if (playersNumber < gameService.MAX_Players) {
                 playersNumber += 1;
-                foreach (var player in players)
-                {
-                    if (player.activeInHierarchy == false)
-                    {
+                foreach (var player in players) {
+                    if (player.activeInHierarchy == false) {
                         player.SetActive(true);
                         break;
                     }
                 }
+                if (playersNumber == gameService.MAX_Players) {
+                    addPlayer.SetActive(false);
+                    removePlayer.SetActive(true);
+                }
             }
+
         }
 
-        public void DeletePlayerInput()
-        {
-            if (playersNumber > gameService.MIN_Players)
-            {
+        public void DeletePlayerInput() {
+            if (playersNumber > gameService.MIN_Players) {
                 playersNumber -= 1;
-                foreach (var player in players.Reverse())
-                {
-                    if (player.activeInHierarchy == true)
-                    {
+                foreach (var player in players.Reverse()) {
+                    if (player.activeInHierarchy == true) {
                         player.SetActive(false);
                         break;
                     }
                 }
+                if (playersNumber == gameService.MIN_Players) {
+                    removePlayer.SetActive(false);
+                    addPlayer.SetActive(true);
+                }
             }
         }
 
-        private void OnDestroy()
-        {
+        private void OnDestroy() {
             DefinePlayers.PlayButton.Click -= OnPlayButton;
         }
     }
--- Assets/Scripts/UI/Menu/StepView.cs
+++ Assets/Scripts/UI/Menu/StepView.cs
@@ -2,31 +2,27 @@ using UnityEngine;
 using UnityEngine.Events;
 
 [RequireComponent(typeof(CanvasGroup))]
-public class StepView : MonoBehaviour
-{
+public class StepView : MonoBehaviour {
     public UnityEvent CompleteHide;
     private CanvasGroup _canvasGroup;
 
-    public void Hide()
-    {
+    public void Hide() {
         if (LeanTween.isTweening(gameObject))
             return;
         _canvasGroup = GetComponent<CanvasGroup>();
         _canvasGroup.alpha = 1;
-        LeanTween.alphaCanvas(_canvasGroup, 0, 1).setEaseInOutCirc().setOnComplete(_ =>
-        {
+        LeanTween.alphaCanvas(_canvasGroup, 0, 0.5f).setEaseInOutCirc().setOnComplete(_ => {
             CompleteHide?.Invoke();
             gameObject.SetActive(false);
         });
     }
 
-    public void Show()
-    {
+    public void Show() {
         if (LeanTween.isTweening(gameObject))
             return;
         _canvasGroup = GetComponent<CanvasGroup>();
         _canvasGroup.alpha = 0;
         gameObject.SetActive(true);
-        LeanTween.alphaCanvas(_canvasGroup, 1, 1).setEaseOutCubic();
+        LeanTween.alphaCanvas(_canvasGroup, 1, 0.5f).setEaseOutCubic();
     }
 }
